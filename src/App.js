f66b95c3447237cd642239964b9830b9c591f27b
Joshua Dinh <joshuahungdinh@gmail.com>
2020-12-25 08:17:14 -0800

    Added files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import motorway from "./Images/motorway2.mp4";
+import section2 from "./Images/3.jpeg";
 import Navigation from "./Components/Navigation/Navigation";
 import Footer from "./Components/Footer/Footer";
 
@@ -8,27 +9,47 @@ function App() {
     <div className="App">
       <Navigation />
 
-      <div className="banner">
-        <video autoPlay muted loop>
-          <source src={motorway} type="video/mp4" />
-        </video>
-        <div className="banner__title">
-          <h1>Extreme Transportation ..</h1>
-        </div>
+      <video autoPlay muted loop>
+        <source src={motorway} type="video/mp4" />
+      </video>
+      <div className="banner__title animate__animated animate__lightSpeedInRight">
+        <h1>Extreme Transportation ..</h1>
       </div>
       {/* <button className="button__header">Contact Us</button> */}
-      {/* <img className="banner" src={banner} alt="truck" /> */}
-      <section className="section__one">
-        <h1>About Us</h1>
-        <p>
-          Extreme Transportation is a California based, Vehicle Transportation
-          Company. Servicing Southern California and all throughout the USA
-          since 2001. Our professional staff and management ensures you have the
-          best experience while we deliver the best quality service.
-        </p>
+      <section className="section__one ">
+        <div className="section__one-title">
+          <h1>About Us</h1>
+          <p>
+            Extreme Transportation is a California based, Vehicle Transportation
+            Company. Servicing Southern California and all throughout the USA
+            since 2001. Our professional staff and management ensures you have
+            the best experience while we deliver the best quality service.
+          </p>
+        </div>
       </section>
-      <section className="section__two">section2</section>
-      <Footer />
+      <section className="section__two">
+        <img className="section__two-image" src={section2} alt="truck" />
+        <div className="section__two-title">
+          <h1>Commitment You Can Trust</h1>
+          <p>
+            “Extreme Transportation is COMMITTED to the business of Vehicle
+            Transporting Services. We are Committed to serving our Customers. We
+            do what we promise in support of our core values: Quality, Loyalty,
+            Integrity, Honor, and Service.
+          </p>
+        </div>
+      </section>
+      <section className="section__three">
+        <div className="section__three-title">
+          <h1>Core values</h1>
+          <p>
+            These values define company. We will apply these values to our
+            everyday decisions, actions, and activities. Our Reputation is of
+            vital importance to us. We take ourselves and our mission seriously.
+            “
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,16 +1,24 @@
 import React from "react";
 import navigation from "./navigation.css";
 import MenuIcon from "@material-ui/icons/Menu";
+import PhoneIcon from "@material-ui/icons/Phone";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 
 function Navigation() {
   return (
     <div className="nav">
       <div className="nav__menu-icon">
-        <MenuIcon fontSize="large" style={{ color: "white" }} />
+        <MenuIcon fontSize="large" style={{ color: "rgb(0, 45, 114)" }} />
       </div>
       <ul>
-        <li>Gallery</li>
-        <li>Contact</li>
+        <li>
+          <PhotoLibraryIcon />
+          Gallery
+        </li>
+        <li>
+          <PhoneIcon />
+          Contact
+        </li>
       </ul>
     </div>
   );
