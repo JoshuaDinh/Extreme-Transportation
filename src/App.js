d616c3228fc49de23c36b415c5a37fc74a34761d
Joshua Dinh <joshuahungdinh@gmail.com>
2021-01-12 15:13:28 -0800

    added files

--- src/App.js
+++ src/App.js
@@ -10,29 +10,56 @@ import Footer from "./Components/Footer/Footer";
 const App = () => {
   const [contact, setContact] = useState(false);
   const [modal, setModal] = useState(false);
+  const [offset, setOffset] = useState(0);
+  const [animateSectionOne, setAnimateSectionOne] = useState(false);
+  const [animateSectionTwo, setAnimateSectionTwo] = useState(false);
+  const [animateSectionThree, setAnimateSectionThree] = useState(false);
 
+  // Contact modal appears at scroll position above 900px
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 900) {
         setContact(true);
       } else setContact(false);
     });
-    // return () => {
-    //   window.removeEventListener("scroll");
-    // };
+  }, []);
+
+  // scroll animation for section / headers
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 900) {
+        setAnimateSectionOne(true);
+      }
+      if (window.scrollY > 1800) {
+        setAnimateSectionTwo(true);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    function handleScroll() {
+      setOffset(window.pageYOffset);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
     <div className="App">
       <Navigation setModal={setModal} modal={modal} />{" "}
-      <div className="banner__title ">
-        <h1 className="animate__animated animate__lightSpeedInRight">
-          Auto Transport & Car Shipping
-        </h1>
-        <p>
-          Get Car Shipping Quote • Best Rated Vehicle Transport Company •
-          Nationwide
-        </p>
+      <div className="bkg">
+        <div className="banner__title ">
+          <div className="background"></div>
+          <h1 className="animate__animated animate__lightSpeedInRight">
+            Auto Transport & Car Shipping
+          </h1>
+          <p>
+            Get Car Shipping Quote • Best Rated Vehicle Transport Company •
+            Nationwide
+          </p>
+        </div>
       </div>
       <div className="sub__headline-container">
         <h3>Auto Transport you can trust</h3>
@@ -56,8 +83,8 @@ const App = () => {
       )}{" "}
       <section className="section__one">
         <div className="section__one-title">
-          <h1>About Us</h1>
-          <p>
+          <h1 className={animateSectionOne && "animation-header"}>About Us</h1>
+          <p className={animateSectionOne && "animation-header"}>
             Extreme Transportation is a California based, Vehicle Transportation
             Company. Servicing Southern California and all throughout the USA
             since 2001. Our professional staff and management ensures you have
@@ -66,10 +93,18 @@ const App = () => {
         </div>
       </section>
       <section className="section__two">
-        <img className="section__two-image" src={section2} alt="truck" />
+        <img
+          className={`section__two-image ${
+            animateSectionTwo && "animation-header"
+          }`}
+          src={section2}
+          alt="truck"
+        />
         <div className="section__two-title">
-          <h1>Commitment You Can Trust</h1>
-          <p>
+          <h1 className={animateSectionTwo && "animation-header"}>
+            Commitment You Can Trust
+          </h1>
+          <p className={animateSectionTwo && "animation-header"}>
             “Extreme Transportation is COMMITTED to the business of Vehicle
             Transporting Services. We are Committed to serving our Customers. We
             do what we promise in support of our core values: Quality, Loyalty,
